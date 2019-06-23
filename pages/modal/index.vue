a8fd456ad35f45c5c72f2efe70dbf20eed1c86d4
06-de15 <06.de15@gmail.com>
2019-06-23 18:02:21 +0900

    clickボタンを作ってみた

--- pages/modal/index.vue
+++ pages/modal/index.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <b-button v-b-modal.modal-1>Launch demo modal</b-button>
-
-    <b-modal id="modal-1" title="BootstrapVue">
-      <p class="my-4">Hello from modal!</p>
-    </b-modal>
-  </div>
+  <button @click="handleClick">Click</button>
 </template>
 <script>
 export default {}
