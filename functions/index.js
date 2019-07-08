a41a5fb6a8027d6ca13889cb260e4f867b8cd97a
06-de15 <06.de15@gmail.com>
2019-07-08 23:18:34 +0900

    firebase設定中

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,27 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions')
+const express = require('express')
+const { Nuxt } = require('nuxt')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express()
+
+const config = {
+  dev: false,
+  debug: false,
+  buildDir: 'nuxt',
+  build: {
+    publicPath: '/public/'
+  }
+}
+const nuxt = new Nuxt(config)
+
+async function handleRequest(req, res) {
+  res.set('Cache-Control', 'public, max-age=300, s-maxage=600')
+  await nuxt.ready() // ← nuxt.ready()でawaitしないといけなくなった！！
+
+  return await nuxt.render(req, res) // eslint-disable-line
+}
+
+app.get('*', handleRequest)
+
+exports.nuxtApp = functions.https // .region('asia-northeast1') // TODO firebase hostingが東京リージョン対応したら嬉しいな
+  .onRequest(app)
