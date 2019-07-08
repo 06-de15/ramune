3591702d20fb17e3bb0575e63eb86a1cb5c67c59
xsota <git@xsota.com>
2019-07-09 00:54:42 +0900

    deleted unused action

--- store/weather.js
+++ store/weather.js
@@ -9,14 +9,7 @@ export const getters = {
 }
 
 // 非同期処理ができるメソッド郡
-export const actions = {
-  async fetchWeatherReport({ commit }) {
-    const weatherResponse = await this.$axios.$get(
-      'https://api.openweathermap.org/data/2.5/forecast?q=Sendai&units=metric&APPID=f409941d71057b26fbb04ad1858159f8'
-    )
-    commit('setList', weatherResponse.list)
-  }
-}
+export const actions = {}
 
 // stateを変更できる唯一の神様
 export const mutations = {
