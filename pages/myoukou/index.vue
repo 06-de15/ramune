eea7f1d1df3ab440c6387c00ea1f30379e44c1e1
06-de15 <06.de15@gmail.com>
2019-08-07 18:13:44 +0900

    タイポミスがあったようだ

--- pages/myoukou/index.vue
+++ pages/myoukou/index.vue
@@ -5,14 +5,52 @@
         妙高高原
       </h1>
       <p class="caution">世界標準時なので＋9時間してください</p>
+      <weather-list :weather-list="list" />
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import WeatherList from '~/components/WeatherList'
+
 export default {
-  name: 'MyoukouVue'
+  components: { WeatherList },
+  component: {
+    WeatherList
+  },
+  computed: {
+    ...mapGetters({
+      list: 'weather/list'
+    })
+  },
+  async fetch({ app, store }) {
+    const weatherResponse = await app.$axios.$get(
+      'https://api.openweathermap.org/data/2.5/forecast?q=Arai&units=metric&APPID=f409941d71057b26fbb04ad1858159f8'
+    )
+
+    store.commit('weather/setList', weatherResponse.list)
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
