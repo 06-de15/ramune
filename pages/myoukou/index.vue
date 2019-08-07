4bd5f629af11dd38992daf0f9e3d8fd9fcf28caf
06-de15 <06.de15@gmail.com>
2019-08-07 18:13:07 +0900

    妙高高原のページの作成とnpm i の副産物

---
+++ pages/myoukou/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        妙高高原
+      </h1>
+      <p class="caution">世界標準時なので＋9時間してください</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyoukouVue'
+}
+</script>
+
+<style scoped></style>
